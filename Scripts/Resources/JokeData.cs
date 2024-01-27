70d429cb8d25fffc4609022a61fadd45c1d8da93
Joe Taylor <CoderJet@gmail.com>
2024-01-27 10:33:02 +0000

    Update joke structure data.

--- Scripts/Resources/JokeData.cs
+++ Scripts/Resources/JokeData.cs
@@ -16,7 +16,7 @@ public partial class JokeData : Resource
 	}
 	
 	[ExportCategory("Joke Data")] 
-	[Export] public string Name;
+	[Export] public string Text;
 	[Export] public JokeType Type;
 	[Export] public JokeColour Colour;
 	[Export] public int ImpactFactor = 1;
