fc820e8909b37eabfdcdd333b3db7280f9af32fe
Aiyon <tangeandfwist@gmail.com>
2024-01-27 13:00:35 +0000

    Mockup UI / Topic entities

--- Scripts/Managers/GameManager.cs
+++ Scripts/Managers/GameManager.cs
@@ -4,7 +4,6 @@ using System;
 public partial class GameManager : Node
 {
 	[Export] public CrowdManager crowdManager;
-	[Export] public 
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
