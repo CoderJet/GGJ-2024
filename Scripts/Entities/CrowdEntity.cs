358ae10256364c00394eb739ef2079675b0fcb85
Aiyon <tangeandfwist@gmail.com>
2024-01-27 18:06:30 +0000

    Crowd faces (bodies pending)

--- Scripts/Entities/CrowdEntity.cs
+++ Scripts/Entities/CrowdEntity.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class CrowdEntity : Node2D
 {
@@ -8,20 +9,35 @@ public partial class CrowdEntity : Node2D
 
 	public List<MoodletData> moodletData;
 
+    [Export] Sprite2D Body;
+    [Export] Sprite2D Head;
+
+    [Export] Texture2D[] Bodies;
+    [Export] Texture2D[] Heads;
+    
 	public void SetValue(int value)
 	{
 		TempValue = value;
-	}
+	}	
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+		RandomizeBody();
+
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+	public void RandomizeBody()
+    {
+        Random rand = new Random();
+        Body.Texture = Bodies[rand.Next(0, Bodies.Count())];
+        Head.Texture = Heads[rand.Next(0, Heads.Count())];
+    }
 
 	public void GeneratePersonality()
 	{
